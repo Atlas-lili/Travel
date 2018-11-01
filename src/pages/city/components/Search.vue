91a486a154c5555532df3388604dabafb0a61a52
Atlas-lili <32982388+Atlas-lili@users.noreply.github.com>
2018-11-01 19:54:14 +0800

    add search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,25 +1,82 @@
 <template>
-    <div class="search">
-      <input class="search-input" type="text" placeholder="请输入城市名称或拼音">
+  <div class="search">
+    <div class="search-body">
+      <input class="search-input" type="text" placeholder="请输入城市名称或拼音" v-model="keyword">
     </div>
+    <div class="search-list" v-show="keyword" ref="search">
+      <ul>
+        <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配信息</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null,
+      searched: false// 匹配是否完成
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length && this.searched
+    }
+  },
+  mounted () {
+    if (this.$refs.search) {
+      console.log(1)
+    }
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      this.searched = false
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        this.searched = true
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+        this.searched = true
+      }, 100)
+    }
+  },
+  props: {
+    cities: Object
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   @import '~styles/varibles';
-  .search {
+  .search-body{
     overflow: hidden;
     height: rem(36);
     text-align: center;
     padding: 0 rem(3);
     background-color: $bg_color;
     font-size: rem(15);
-    &-input{
+    .search-input{
       box-sizing: border-box;
       outline: none;
       border: none;
@@ -30,6 +87,23 @@ export default {
       font-size: rem(15);
       text-align: center;
       color: #333;
+    }
+  }
+  .search-list{
+    z-index: $little;
+    overflow: hidden;
+    position: absolute;
+    top: rem(79);
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #eee;
+    .search-item{
+      line-height: rem(30);
+      font-size: rem(16);
+      color: #666;
+      padding-left: rem(8);
+      background-color: #fff;
     }
   }
 </style>

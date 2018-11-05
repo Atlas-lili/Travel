1c6c14cd0d09da8350ac588b1b1da05c3f5ca725
Atlas-lili <32982388+Atlas-lili@users.noreply.github.com>
2018-11-05 15:32:45 +0800

    add detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,69 @@
     <div class="detail">
       <detail-banner></detail-banner>
       <detail-header></detail-header>
-      <div class="contant"></div>
+      <div class="contant">
+        <detail-list :list="list"></detail-list>
+      </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆某一'
+          }, {
+            title: '成人三馆某一'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }, {
+          title: '成人八馆联票'
+        }]
+      }, {
+        title: '学生票',
+        children: [{
+          title: '成人三馆联票'
+        }, {
+          title: '成人五馆联票'
+        }, {
+          title: '成人八馆联票'
+        }]
+      }, {
+        title: '儿童票',
+        children: [{
+          title: '成人三馆联票'
+        }, {
+          title: '成人五馆联票'
+        }, {
+          title: '成人八馆联票'
+        }]
+      }, {
+        title: '特惠票',
+        children: [{
+          title: '成人三馆联票'
+        }, {
+          title: '成人五馆联票'
+        }, {
+          title: '成人八馆联票'
+        }]
+      }]
+    }
+  },
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
